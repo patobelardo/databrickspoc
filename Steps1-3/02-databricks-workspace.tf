2a9f03fdd9a48feae67f2ab843b622fe3f04669c
Patricio Belardo <patobelardo@outlook.com>
2021-02-18 13:12:25 -0500

    fmt

--- Steps1-3/02-databricks-workspace.tf
+++ Steps1-3/02-databricks-workspace.tf
@@ -13,7 +13,7 @@ resource "azurerm_databricks_workspace" "dbricks" {
 
 provider "databricks" {
   azure_workspace_resource_id = azurerm_databricks_workspace.dbricks.id
-  azure_client_id = var.client_id
-  azure_client_secret = var.client_secret
-  azure_tenant_id = var.tenant_id
+  azure_client_id             = var.client_id
+  azure_client_secret         = var.client_secret
+  azure_tenant_id             = var.tenant_id
 }
